caa289d27c4ea5741cee226a7ba4dcc53f7d25b2
amhyou <younes.amhaouch@hotmail.com>
2024-07-17 16:25:13 +0100

    total expense string

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,6 +89,6 @@
     <div
         class="fixed bottom-0 left-0 right-0 p-4 bg-gray-800 text-white text-center"
     >
-        Total Expense today: {total} $
+        Total Expenses: {total} $
     </div>
 </center>
